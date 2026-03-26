dc6faddae414097131b4771ff9d825d724ce2564
harryitc <harryitc.dev@gmail.com>
2026-03-26 13:44:34 +0700

    remove style

--- web-client/admin/app/admin/(dashboard)/layout.tsx
+++ web-client/admin/app/admin/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
   return (
     <Layout className="min-h-screen flex flex-row">
       <AdminSidebar />
-      <Layout className="md:ml-[260px] transition-all bg-gray-50 flex-grow">
+      <Layout className="transition-all bg-gray-50 flex-grow">
         <AdminHeader />
         <Content className="p-8">
           {children}
